851b752908c10e09b28a61f0df49cefaced9dd3f
micha12344 <52766908+micha12344@users.noreply.github.com>
2019-08-18 01:54:16 +0300

    Delete connection.js

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-//connect mongodb
-mongoose.connect('mongodb+srv://:@cluster0-ngaul.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
-const db = mongoose.connection;
-
-db.on('error', (error) => console.log('db connection err'))
-
-db.once('open', () => console.log('connected to mongo db'))
